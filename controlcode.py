b872bc06548b5c044494c1774a28d11b394b5d43
Gaben <46634297+gaben5841@users.noreply.github.com>
2021-04-11 13:34:14 -0400

    Update controlcode.py

--- controlcode.py
+++ controlcode.py
@@ -1,17 +1,39 @@
 from Raspi_MotorHAT import Raspi_MotorHAT
 import keyboard
+
 mh = Raspi_MotorHAT(addr=0x6f)
-motor = mh.getMotor(1)
-motor.run(Raspi_MotorHAT.FORWARD)
-#loopend = True
+lmotor = mh.getMotor(1)
+rmotor = mh.getMotor(2)
+#lmotor.run(Raspi_MotorHAT.FORWARD)
+# loopend = True
 
 while True:
-    
+
     if keyboard.is_pressed('w'):
-        motor.setSpeed(100)    
+        lmotor.run(Raspi_MotorHAT.FORWARD)
+        rmotor.run(Raspi_MotorHAT.FORWARD)
+        lmotor.setSpeed(100)
+        rmotor.setSpeed(100)
+    elif keyboard.is_pressed('s'):
+        lmotor.run(Raspi_MotorHAT.BACKWARD)
+        rmotor.run(Raspi_MotorHAT.BACKWARD)
+        lmotor.setSpeed(100)
+        rmotor.setSpeed(100)
+    elif keyboard.is_pressed('a'):
+        lmotor.run(Raspi_MotorHAT.FORWARD)
+        rmotor.run(Raspi_MotorHAT.BACKWARD)
+        lmotor.setSpeed(100)
+        rmotor.setSpeed(100)
+    elif keyboard.is_pressed('d'):
+        lmotor.run(Raspi_MotorHAT.BACKWARD)
+        rmotor.run(Raspi_MotorHAT.FORWARD)
+        lmotor.setSpeed(100)
+        rmotor.setSpeed(100)
     else:
-        motor.setSpeed(0)
-    
+        lmotor.setSpeed(0)
+        rmotor.setSpeed(0)
+
     if keyboard.is_pressed('q'):
         break
+
     
