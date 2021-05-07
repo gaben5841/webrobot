28c5e912fc8c929fba7e17387f90d57b2ad5bb1c
Gaben <46634297+gaben5841@users.noreply.github.com>
2021-05-07 07:17:16 -0400

    serverclient w base64/ binary data transfer

--- serverclient.py
+++ serverclient.py
@@ -1,17 +1,32 @@
 import websockets
 import asyncio
 import io
+import base64
 from threading import Condition
+
 
 
 async def hello():
     uri = "ws://192.168.7.85:3030"
     async with websockets.connect(uri) as websocket:
-        message = input("What would you like to send to the server? ")
-        await websocket.send(message)
+        #message = input("What would you like to send to the server? ")
+        await websocket.send(img2bytes("YO_M.jpg"))
         #await websocket.send
         response = await websocket.recv()
         print(response)
+
+def img2base64(img):
+    with open(img, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        print(encoded_string)
+    return encoded_string
+
+def img2bytes(img):
+    with open(img, "rb") as image:
+        f = image.read()
+        b = bytearray(f)
+        return b
+
 
 class streamer(object):
     def __init__(self):
