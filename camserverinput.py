46edd0fd7e1105c0b351d96f975d90b7e0786084
Gaben <46634297+gaben5841@users.noreply.github.com>
2021-05-23 17:14:33 -0400

    this

--- camserverinput.py
+++ camserverinput.py
@@ -1,120 +1,47 @@
-import io
-import picamera
+from io import BytesIO
+from picamera import PiCamera
+import threading
 import logging
-import socketserver
+import websockets
+import base64
+import asyncio
+#import cv2 as cv
+import numpy
 from threading import Condition
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from robotdrive import driveBot
+import time
 
-PAGE = """\
-<html>
-    <head>
-        <title>Raspberry Pi - Surveillance Camera</title>
-    </head>
-    <body>
-        <center><h1>Raspberry Pi - Surveillance Camera</h1></center>
-        <center><img src="stream.mjpg" width="640" height="480"></center>
-        <form action="/" method="POST">
-            <input type="submit" name="forward" value="w">
-            <input type="submit" name="left" value="a">
-            <input type="submit" name="backward" value="s">
-            <input type="submit" name="right" value="d">
-            <input type="submit" name="stop" value="STOP">
-        </form>
-    </body>
-</html>
-"""
+uri = "ws://192.168.7.85:3030"
+my_stream = BytesIO()
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 15
+msg = ""
 
+async def recieveMsg(ws):
+    print('waiting for response')
+    msg = await ws.recv()
+    await msg
+    print(msg)
 
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
+async def socketSend(ws, message):
+    for foo in camera.capture_continuous(my_stream, format = 'jpeg', use_video_port=True):
+        await ws.send(message.getvalue())
+        print('sending message')
+        asyncio.create_task(recieveMsg(ws))
+        my_stream.seek(0)
+        if time.time() - start > 20:
+            break
+        my_stream.seek(0)
+        my_stream.truncate()
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
+start = time.time()
 
+loop = asyncio.get_event_loop()
 
-class StreamingHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-
-    def _redirect(self, path):
-        self.send_response(303)
-        self.send_header('Content-type', 'text/html')
-        self.send_header('Location', path)
-        self.end_headers()
-
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])  # Get the size of data
-        post_data = self.rfile.read(content_length).decode("utf-8")  # Get the data
-        post_data = post_data.split("=")[1]  # Only keep the value
-
-        driveBot(post_data)
-        post_data = " "
-        self._redirect("/")
-
-8
-class StreamingServer(socketserver.ThreadingMixIn, HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
-
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    # Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    # camera.rotation = 90
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
+async def main() :
+    ws = await websockets.connect(uri)
+    
+    sender = asyncio.create_task(socketSend(ws, my_stream))
+    await sender
+        
+loop.run_until_complete(main())
